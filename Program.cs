e63aeac788dde04338bd15c295fff5c2b963bca2
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-09-03 22:48:42 +0300

    сумма чисел на нечетных позициях

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int size = 4;
+int [] array = new int [size];
+for(int i=0;i<array.Length;i++)
+{
+    array[i] = new Random().Next(-100,100);}
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 == 1)
+        sum += array[i];
+}
+Console.Write("[");
+Console.Write(string.Join("," , array));
+Console.Write("] => ");
+Console.WriteLine($"Сумма  чисел на нечетных позициях = {sum}");
+
